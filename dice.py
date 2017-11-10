9f662df396236fd0328f52fcb5bd52ed23bba14b
Woozsh <jordy.pereira@hotmail.com>
2017-11-10 23:28:08 +0100

    rolls X times

--- dice.py
+++ dice.py
@@ -6,7 +6,16 @@ class Dice:
 
     def roll(self):
         roll = random.randint(0, self._eyes)
-        return roll
+        return str(roll)
+    def rollX(self, x = 1):
+        rolls = []
+        for i in range(0, x):
+            rolls.append(random.randint(0, self._eyes))
+        return rolls
 
-dice = Dice()
-print dice.roll()
+    def printRoll(self, x = 1):
+        rolls = self.rollX(x)
+        print " -------------------"
+        for i, val in enumerate(rolls):
+            print "|  Roll #" + str(i+1) + "     " + str(val) + "    |"
+        print " -------------------"
